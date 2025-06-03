65c695047da250887a78525093e9f3cb81b7a9e5
DaniilKalts <kaltsdaniil@gmail.com>
2025-06-03 18:43:24 +0500

    feat: add updates listener

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"log"
 	"os"
 
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 )
 
@@ -12,5 +14,33 @@ func main() {
 	token := os.Getenv("BOT_TOKEN")
 	if token == "" {
 		panic("Нужен токен от телеграм-бота для запуска. Укажите в переменных окружения (.env)")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bot.Debug = true // чтобы подробно видеть логи
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 30
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil || update.Message.Text == "" {
+			continue
+		}
+
+		chatID := update.Message.Chat.ID
+		userInput := update.Message.Text
+
+		if userInput == "/start" {
+			message := tgbotapi.NewMessage(
+				chatID,
+				"Привет! Нужно вычислить африметическое выражение?\n\nТы по адрессу!\n\nНабери любой пример и я его решу!",
+			)
+			bot.Send(message)
+			continue
+		}
 	}
 }
